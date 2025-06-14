672983e6c9d499084172dca225d82a1af6316452
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-06-14 15:31:42 -0300

    initial commit

---
+++ api/processPayment.ts
@@ -0,0 +1,46 @@
+// api/processPayment.js
+
+import axios from "axios";
+
+const MERCADO_PAGO_ACCESS_TOKEN = process.env.MERCADO_PAGO_ACCESS_TOKEN;
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Método não permitido" });
+    return;
+  }
+
+  const {
+    token,
+    transactionAmount,
+    description,
+    installments,
+    paymentMethodId,
+    payer,
+  } = req.body;
+
+  try {
+    const response = await axios.post(
+      "https://api.mercadopago.com/v1/payments",
+      {
+        token,
+        transaction_amount: Number(transactionAmount),
+        description,
+        installments,
+        payment_method_id: paymentMethodId,
+        payer,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${MERCADO_PAGO_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error.response?.data || error.message || error);
+    res.status(500).json({ error: "Erro ao processar pagamento" });
+  }
+}
