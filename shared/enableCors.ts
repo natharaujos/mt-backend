eb67bc920d108889678777e48985b324965a3a98
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-06-15 18:34:04 -0300

    enabling production url

--- shared/enableCors.ts
+++ shared/enableCors.ts
@@ -5,7 +5,7 @@ export default function enableCors(
   req: VercelRequest
 ): boolean {
   const allowedOrigins = [
-    "https://maguinha-frontend.vercel.app",
+    "https://maguinhatuquinho.web.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
   ];
