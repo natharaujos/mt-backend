537075313301e075c22861ddfbe8365a04331c47
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-06-15 12:34:46 -0300

    Add GitHub Action for Vercel deploy on main branch

--- shared/enableCors.ts
+++ shared/enableCors.ts
@@ -12,7 +12,6 @@ export default function enableCors(res: VercelResponse, req: VercelRequest) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
