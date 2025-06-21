00dae3e69cdbc8977c2602d188031debcbe83eac
Nathan Araujo <nathan.araujo@dtidigital.com.br>
2025-06-21 12:17:25 -0300

    adjusting auto return status

--- api/createPreferencePix.ts
+++ api/createPreferencePix.ts
@@ -61,8 +61,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
           excluded_payment_types: [{ id: 'credit_card' }, { id: 'debit_card' }, { id: 'ticket' }],
           default_payment_method_id: 'pix',
         },
-        back_urls: body.back_urls,
-        auto_return: 'approved',
+        auto_return: 'pending',
         binary_mode: true,
       },
       {
